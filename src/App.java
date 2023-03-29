67dffece3b842697961f99f28fa183853a596ecc
DanielOiveira <danielcompositor1307@gmail.com>
2023-03-28 22:50:35 -0300

    Aula 2 - Gerando figurinhas para Whatsapp

--- src/App.java
+++ src/App.java
@@ -1,13 +1,16 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
 public class App {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
         // Requisição HTTP para buscar dados do TOP 250 filmes do IMDB
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         var address = URI.create(url);
@@ -23,11 +26,17 @@ public class App {
 
         //Exibir e manipular dados coletados
 
+        var generator = new StickerGenerator();
+
         for (Map<String, String> movie: listMovies) {
             System.out.println(movie.get("title"));
             System.out.println(movie.get("image"));
             System.out.println(movie.get("imDbRating"));
             System.out.println();
+
+           //Gerando figurinhas com base na URL das imagens
+            InputStream inputStream = new URL(movie.get("image")).openStream();
+            generator.createSticker(inputStream, movie.get("title"));
         }
     }
 }
---
+++ src/StickerGenerator.java
@@ -0,0 +1,38 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+
+public class StickerGenerator {
+
+   public void createSticker(InputStream inputStream, String fileName) throws Exception {
+        //Leitura da imagem
+         // InputStream inputStream = new URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_1.jpg").openStream();
+        //InputStream inputStream = new FileInputStream(new File("entrada/filme.jpg"));
+        BufferedImage originalImage =  ImageIO.read(inputStream);
+
+        //Criar nova imagem em memória com transparência e com tamanho novo
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight();
+        int newHeight = height + 200;
+        BufferedImage newImage = new BufferedImage(width,newHeight, BufferedImage.TRANSLUCENT);
+
+        //Copiar imagem original para nova imagem (em memória)
+        Graphics2D graphics = (Graphics2D) newImage.getGraphics();
+        graphics.drawImage(originalImage, 0, 0, null);
+
+        //Configuração fonte
+        graphics.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 64));
+        graphics.setColor(Color.ORANGE);
+
+        //Escrever uma frase na nova imagem
+        graphics.drawString("TOPZERA", 200, newHeight - 100);
+
+        //Escrever nova imagem em um arquivo
+        ImageIO.write(newImage, "png", new File("saida/" + fileName + ".png"));
+    }
+}
